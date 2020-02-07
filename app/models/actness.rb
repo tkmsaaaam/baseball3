056a93f0c05044f52590d5b6586d0c7fd22b1c3e
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-02-07 15:55:03 +0900

    made validate

--- app/models/actness.rb
+++ app/models/actness.rb
@@ -1,3 +1,5 @@
 class Actness < ApplicationRecord
   belongs_to :Situation
+  validates :ball_type, presence: true,length: {maximum: 2}
+  validates :ball_strike, presence: true,length: {minimum: 2,maximum: 2}
 end
