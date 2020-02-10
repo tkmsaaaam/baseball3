80ca0c8a06e9517ae68de24833c392267d6ad5c6
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-02-10 15:44:34 +0900

    IDの表示部分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :runnings
-  resources :actnesses
-  resources :situations
   resources :games
+  resources :situations
+  resources :actnesses
+  resources :runnings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
