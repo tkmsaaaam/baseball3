b8de46c701ff77ab29c6b251aa15b5cb0e23bbbe
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-02-21 19:22:36 +0900

    Default No Set

--- app/controllers/actnesses_controller.rb
+++ app/controllers/actnesses_controller.rb
@@ -16,7 +16,7 @@ class ActnessesController < ApplicationController
   def new
     puts params[:situation_id]
     situation = Situation.find(params[:situation_id])
-    @actness = Actness.new(situation_id: params[:situation_id])
+    @actness = Actness.new(situation_id: params[:situation_id],ball_type: 1,ball: true,ball_strike: 11)
     @situation = Situation.find(params[:situation_id])
   end
 
