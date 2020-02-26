781c141dd9aa32b52117abee6223940e0529a27d
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-02-26 19:53:52 +0900

    schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_060045) do
+ActiveRecord::Schema.define(version: 2020_02_26_104059) do
 
   create_table "actnesses", force: :cascade do |t|
     t.integer "ball_type"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 2020_02_26_060045) do
     t.boolean "strike"
     t.boolean "foul"
     t.integer "ball_strike"
-    t.integer "situation_id"
+    t.integer "Situation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["situation_id"], name: "index_actnesses_on_Situation_id"
+    t.index ["situation_id"], name: "index_actnesses_on_situation_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_060045) do
     t.integer "situation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["situation_id"], name: "index_runnings_on_Situation_id"
+    t.index ["situation_id"], name: "index_runnings_on_situation_id"
   end
 
   create_table "situations", force: :cascade do |t|
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_060045) do
     t.integer "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_situations_on_Game_id"
+    t.index ["game_id"], name: "index_situations_on_game_id"
   end
 
 end
