3f37ccbecd8a306804d5309f422f6f8b6ac9ca16
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-05-02 10:36:13 +0900

    add element ui

---
+++ app/javascript/packs/games/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import GameIndex from '../../games/index.vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI, { locale })
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        render: h => h(GameIndex)
+    }).$mount()
+    document.body.appendChild(app.$el)
+})
