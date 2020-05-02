8cad133359d03f7c41da8239bee7563b6b609013
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2020-05-02 14:46:55 +0900

    rewrite game index by vue.js

--- app/javascript/games/index.vue
+++ app/javascript/games/index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-table
-            :data="tableData"
+            :data="games"
             style="width: 100%">
         <el-table-column
                 prop="date"
@@ -8,38 +8,43 @@
                 width="180">
         </el-table-column>
         <el-table-column
-                prop="name"
-                label="Name"
+                prop="id"
+                label="ID"
                 width="180">
         </el-table-column>
         <el-table-column
-                prop="address"
-                label="Address">
+            width="120">
+            <template v-slot="scope">
+                <el-button
+                        @click="destroyGame(scope.row.id)"
+                        type="danger"
+                        icon="el-icon-delete"
+                        circle></el-button>
+            </template>
         </el-table-column>
     </el-table>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                tableData: [{
-                    date: '2016-05-03',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles'
-                }, {
-                    date: '2016-05-02',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles'
-                }, {
-                    date: '2016-05-04',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles'
-                }, {
-                    date: '2016-05-01',
-                    name: 'Tom',
-                    address: 'No. 189, Grove St, Los Angeles'
-                }]
+    import axios from 'axios'
+    import {reject} from 'lodash';
+        export default {
+            data() {
+                return {
+                    games: []
+                }
+            },
+            created(){
+                axios.get('api/v1/games')
+                    .then(res => {
+                        this.games = res.data
+                    })
+            }
+            methods: {
+                destroyGame(id) {
+                    axios.delete('/api/v1/games/' + id)
+                    .then(res => {
+                    });
+                }
             }
         }
-    }
 </script>
