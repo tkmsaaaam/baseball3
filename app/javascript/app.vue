9fab706bf02161bc7e0338aa3830d6f22ce76412
Takamasa Matsui <59013192+tkmsaaaam@users.noreply.github.com>
2020-07-19 12:44:12 +0900

    initialize vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,46 +1,17 @@
 <template>
   <div id="app">
-    <el-form
-            :model="game"
-            :action="game.action"
-            method="post">
-      <el-form-item>
-        <el-input
-                type="hidden"
-                :value="game.csrfToken"
-                name="authenticity_token"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-date-picker
-                type="date"
-                format="yyyy/MM/dd"
-                value-format="yyyy/MM/dd"
-                placeholder="日付を選択"
-                v-model="game.date"
-                name="game[date]"></el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button
-                type="primary"
-                native-type="submit">作成
-        </el-button>
-      </el-form-item>
-    </el-form>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        game: {
-          csrfToken: document.getElementsByName('csrf-token')[0].content,
-          date: '',
-          action: 'games'
-        }
-      }
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
     }
   }
+}
 </script>
 
 <style scoped>
