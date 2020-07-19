610e480bcf40f827dec46cda589fe9fd7a5da8b3
Takamasa Matsui <59013192+tkmsaaaam@users.noreply.github.com>
2020-07-19 17:11:03 +0900

    delete complete element ui

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import HomeIndex from '../../organisms/home/index.vue'
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI, { locale })
-
 
 document.addEventListener('DOMContentLoaded', () => {
+    const el = document.body.appendChild(document.createElement('hello'))
     const app = new Vue({
+        el,
         render: h => h(HomeIndex)
-    }).$mount()
-    document.body.appendChild(app.$el)
+    })
 
     console.log(app)
 })
