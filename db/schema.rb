d2960b9df37a0757f82e07545b24c73d92864189
Takamasa Matsui <59013192+tkmsaaaam@users.noreply.github.com>
2020-08-10 11:33:28 +0900

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_045923) do
+ActiveRecord::Schema.define(version: 2020_08_10_015305) do
 
   create_table "actnesses", force: :cascade do |t|
     t.integer "ball_type"
@@ -56,6 +56,18 @@ ActiveRecord::Schema.define(version: 2020_02_05_045923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_situations_on_game_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
