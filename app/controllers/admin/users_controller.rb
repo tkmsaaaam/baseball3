40d97e6c028ffb06d3c8ba446acda05f8fed3456
松井隆将 <takamasamatsui@matsuitakashishounoMacBook-Pro.local>
2020-08-14 07:55:18 +0900

    delete original authorize

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,8 +7,6 @@ module Admin
 
     def index
       @users = User.all
-
-      redirect_to home_index_url unless @users.index
     end
 
     def show; end
@@ -43,15 +41,6 @@ module Admin
 
     def user_params
       params.require(:user).permit(:id, :email)
-    end
-
-    def check_user_role
-      return admin_access_denine unless user_signed_in?
-      return admin_access_denine unless User::ADMMIN_USER_IDS.include?(current_user.id)
-    end
-
-    def admin_access_denine
-      redirect_to home_index_url
     end
 
     def check_authorize
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  ADMMIN_USER_IDS = [1].freeze
 
   has_many :game
   enum user_type: { normal: 1, admin: 10 }
