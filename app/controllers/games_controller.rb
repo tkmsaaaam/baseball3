350585c64fe16cb4973225d8c51bbf16aa19ddd9
松井隆将 <takamasamatsui@matsuitakashishounoMacBook-Pro.local>
2020-08-30 20:07:08 +0900

    Update games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -72,6 +72,6 @@ class GamesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def game_params
-    params.require(:game).permit(:date)
+    params.require(:game).permit(:date, :user_id)
   end
 end
