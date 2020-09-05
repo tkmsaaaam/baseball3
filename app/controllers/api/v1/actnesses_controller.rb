342b9962f104d9633d089c82f4b9f6a9edfd054e
松井隆将 <takamasamatsui@matsuitakashishounoMacBook-Pro.local>
2020-09-05 10:45:16 +0900

    add actnesses

--- app/controllers/api/v1/actnesses_controller.rb
+++ app/controllers/api/v1/actnesses_controller.rb
@@ -8,6 +8,11 @@ module Api
         render json: actnesses
       end
 
+      def show
+        actness = Actness.select(:id, :ball_type, :swing, :ball, :strike, :foul, :ball_strike).find_by(id: params[:id])
+        render json: actness
+      end
+
       def destroy
         actness = Actness.find_by(id: params[:id])
         render status: 200, json: { status: 200 } if actness.destroy
